a61c0558958689e4a3637ddaa45b208bfdf6b7cc
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2019-04-30 22:00:32 +0100

    Add ncurses with sleep

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <ncurses.h>
+#include <unistd.h>
+
+void app() {
+	wmove(stdscr, 0, 0);
+	waddch(stdscr, 'a');
+	wrefresh(stdscr);
+	sleep(5);
+}
 
 int main() {
-	std::cout << "Works" << std::endl;
+	initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr, TRUE);
+	app();
+	endwin();
 	return 0;
 }
